9ba51b8465c4cd575b70172ef70764506a6256f0
Annie Liew <anniebombanie@gmail.com>
2020-04-30 22:12:24 -0400

    cleaned up html & css

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const panels = document.querySelectorAll( '.panel' );
+const panels = document.querySelectorAll('.panel');
 
 function toggleOpen() {
-  this.classList.toggle( 'open' );
-};
+  console.log('Hello');
+  this.classList.toggle('open');
+}
 
-function toggleActive( e ) {
-  // console.log(e.propertyname);
-
-  if ( e.propertyName.includes( 'flex' )) {
-    this.classList.toggle( 'open-active' );
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
   }
-};
+}
 
-panels.forEach( panel => panel.addEventListener( 'click', toggleOpen ));
-panels.forEach( panel => panel.addEventListener( 'transitionend', toggleActive ));
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
